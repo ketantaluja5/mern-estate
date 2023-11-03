c0645b4a5264015c296d6255967c8283a80d63a3
ketantaluja5 <ketantaluja5@gmail.com>
2023-11-03 12:51:00 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const app = express();
 
 app.listen(3000, () => {
